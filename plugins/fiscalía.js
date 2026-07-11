832ed62096ae958dc7af290eaae98d9acbb9080d
Alexander12Po <alexanderpaulhuamanramos25@gmail.com>
2026-07-11 10:57:30 -0500

    Create fiscalía.js

---
+++ plugins/fiscalía.js
@@ -0,0 +1,19 @@
+export async function handler(m, { conn }) {
+  const texto = `
+━━━━━━━━━━━━━━━━━━
+📋 CONSULTA DE DENUNCIAS 
+━━━━━━━━━━━━━━━━━━
+💎 Servicio exclusivo para usuarios Premium.
+━━━━━━━━━━━━━━━━━━
+🧑‍💻 Soporte Técnico
+━━━━━━━━━━━━━━━━━━
+`.trim()
+
+  await conn.reply(m.chat, texto, m)
+}
+
+handler.help = ['telp']
+handler.tags = ['premium']
+handler.command = /^telp$/i
+
+export default handler
