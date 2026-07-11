f9179a407b67efbd7805cbdecbb4f93e5738499e
Alexander12Po <alexanderpaulhuamanramos25@gmail.com>
2026-07-11 11:35:04 -0500

    Update dnielectrónico.js

--- plugins/dnielectrónico.js
+++ plugins/dnielectrónico.js
@@ -1,19 +1,16 @@
-export async function handler(m, { conn }) {
-  const texto = `
-━━━━━━━━━━━━━━━━━━
-📋 CONSULTA DE DNI ELECTRÓNICO 
+export default {
+  command: ['dnielectrónico', 'dnielectronico'],
+  description: 'Consulta DNI Electrónico',
+
+  exec: async ({ sock, from, msg }) => {
+    await sock.sendMessage(from, {
+      text: `━━━━━━━━━━━━━━━━━━
+💳 CONSULTA DNI ELECTRÓNICO
 ━━━━━━━━━━━━━━━━━━
 💎 Servicio exclusivo para usuarios Premium.
 ━━━━━━━━━━━━━━━━━━
 🧑‍💻 Soporte Técnico
-━━━━━━━━━━━━━━━━━━
-`.trim()
-
-  await conn.reply(m.chat, texto, m)
+━━━━━━━━━━━━━━━━━━`
+    }, { quoted: msg })
+  }
 }
-
-handler.help = ['telp']
-handler.tags = ['premium']
-handler.command = /^telp$/i
-
-export default handler
