220860db8d7d1db2fbab934431ad4fd0b2d637b8
Alexander12Po <alexanderpaulhuamanramos25@gmail.com>
2026-07-11 01:07:23 -0500

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -2,7 +2,7 @@ import fs from 'fs'
 import path from 'path'
 import { fileURLToPath } from 'url'
 import { botConfig } from '../config.js'
-import { getUniquePlugins } from '../handler.js'
+const { getUniquePlugins } = await import('../handler.js')
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
