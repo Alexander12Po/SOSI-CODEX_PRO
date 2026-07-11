8c1bf0eb3e8567cb7d279984c06b82818ffc2136
Alexander12Po <alexanderpaulhuamanramos25@gmail.com>
2026-07-11 09:36:37 -0500

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ export const botConfig = {
   botName: 'SOSI CODEX',
   prefix: '.',
   ownerNumber: '521234567890', // 👈 cambia esto por tu número con código de país, sin +
-  menuImage: './media/menu.jpg'
+  menuImage: './media/menu.png',
   loginMethod: 'qr' // 👈 'qr' o 'pairing'
 }
