c2c0bd3b644d2b3e0f22cb08e8b6675ae14145bd
Alexander12Po <alexanderpaulhuamanramos25@gmail.com>
2026-07-11 11:33:44 -0500

    Update fiscalía.js

--- plugins/fiscalía.js
+++ plugins/fiscalía.js
@@ -1,19 +1,16 @@
-export async function handler(m, { conn }) {
-  const texto = `
-━━━━━━━━━━━━━━━━━━
-📋 CONSULTA DE DENUNCIAS 
+export default {
+  command: ['fiscalia'],
+  description: 'Consulta Fiscalía',
+
+  exec: async ({ sock, from, msg }) => {
+    await sock.sendMessage(from, {
+      text: `━━━━━━━━━━━━━━━━━━
+⚖️ CONSULTA FISCALÍA
 ━━━━━━━━━━━━━━━━━━
 💎 Servicio exclusivo para usuarios Premium.
 ━━━━━━━━━━━━━━━━━━
 🧑‍💻 Soporte Técnico
-━━━━━━━━━━━━━━━━━━
-`.trim()
-
-  await conn.reply(m.chat, texto, m)
+━━━━━━━━━━━━━━━━━━`
+    }, { quoted: msg })
+  }
 }
-
-handler.help = ['telp']
-handler.tags = ['premium']
-handler.command = /^telp$/i
-
-export default handler
