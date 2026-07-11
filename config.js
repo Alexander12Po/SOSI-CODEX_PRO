cca21d23bef62eb2faddf55f4acbd46c9f05b829
Alexander12Po <alexanderpaulhuamanramos25@gmail.com>
2026-07-11 17:10:47 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
 export const botConfig = {
   botName: 'SOSI CODEX',
   prefix: '.',
-  ownerNumber: '521234567890', // 👈 cambia esto por tu número con código de país, sin +
+  ownerNumber: '51924894999', // 👈 tu número con código de país, sin +
   menuImage: './media/menu.png',
-  loginMethod: 'qr' // 👈 'qr' o 'pairing'
+  loginMethod: 'qr', // 👈 'qr' o 'pairing'
+  admins: ['51924894999@s.whatsapp.net']
 }
