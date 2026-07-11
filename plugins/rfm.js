870d1ff165c26c20598100193a8c740031ea1def
Alexander12Po <alexanderpaulhuamanramos25@gmail.com>
2026-07-11 11:35:28 -0500

    Update rfm.js

--- plugins/rfm.js
+++ plugins/rfm.js
@@ -1,19 +1,16 @@
-export async function handler(m, { conn }) {
-  const texto = `
-━━━━━━━━━━━━━━━━━━
-📋 CONSULTA DE RECONOCIMIENTO FACIAL 
+export default {
+  command: ['rfm'],
+  description: 'Consulta RFM',
+
+  exec: async ({ sock, from, msg }) => {
+    await sock.sendMessage(from, {
+      text: `━━━━━━━━━━━━━━━━━━
+📄 CONSULTA RFM
 ━━━━━━━━━━━━━━━━━━
 💎 Servicio exclusivo para usuarios Premium.
 ━━━━━━━━━━━━━━━━━━
 🧑‍💻 Soporte Técnico
-━━━━━━━━━━━━━━━━━━
-`.trim()
-
-  await conn.reply(m.chat, texto, m)
+━━━━━━━━━━━━━━━━━━`
+    }, { quoted: msg })
+  }
 }
-
-handler.help = ['telp']
-handler.tags = ['premium']
-handler.command = /^telp$/i
-
-export default handler
