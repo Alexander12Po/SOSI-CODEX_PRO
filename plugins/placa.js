1597305c13d029b509873a2ad22f184995d75d42
Alexander12Po <alexanderpaulhuamanramos25@gmail.com>
2026-07-11 11:33:21 -0500

    Update placa.js

--- plugins/placa.js
+++ plugins/placa.js
@@ -1,19 +1,16 @@
-export async function handler(m, { conn }) {
-  const texto = `
-━━━━━━━━━━━━━━━━━━
-📋 CONSULTA DE PLACA
+export default {
+  command: ['placa'],
+  description: 'Consulta Placa',
+
+  exec: async ({ sock, from, msg }) => {
+    await sock.sendMessage(from, {
+      text: `━━━━━━━━━━━━━━━━━━
+🚗 CONSULTA DE PLACA
 ━━━━━━━━━━━━━━━━━━
 💎 Servicio exclusivo para usuarios Premium.
 ━━━━━━━━━━━━━━━━━━
 🧑‍💻 Soporte Técnico
-━━━━━━━━━━━━━━━━━━
-`.trim()
-
-  await conn.reply(m.chat, texto, m)
+━━━━━━━━━━━━━━━━━━`
+    }, { quoted: msg })
+  }
 }
-
-handler.help = ['telp']
-handler.tags = ['premium']
-handler.command = /^telp$/i
-
-export default handler
